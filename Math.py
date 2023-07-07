e8169c563345d410329e257b64a1adb3ba365053
Hemanth Thatikonda <hemanththatikonda02@gmail.com>
2023-07-08 01:28:50 +0530

    This is basic addition and division

---
+++ Math.py
@@ -0,0 +1,7 @@
+#Basic math code adding 2 number and divide by 2 
+b=int(input("enter first number: "))
+c=int(input("enter second number: "))
+Add =(b+c)
+print("The addition of two number is:", Add)
+Div=Add/2
+print("The average of sum is:", Div)
